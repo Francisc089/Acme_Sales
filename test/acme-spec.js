e887b202ceb43822c29f248e2ea9651fc87e1a2f
Francisco Munoz <fmunozpena@gmail.com>
2018-07-10 13:39:00 -0400

    completed assignment

---
+++ test/acme-spec.js
@@ -0,0 +1,67 @@
+const expect = require('chai').expect;
+const generateCustomerSalesMap = require('./acme.js');
+const sinon = require('sinon');
+
+const sales = [
+    {customerId : 1,
+    orderId : 1,
+    total: 3},
+    {customerId : 2,
+    orderId : 2,
+    total : 4 }
+];
+
+const customers = [
+    {id : 1,
+    name: 'Moe'},
+    {id : 2,
+    name: 'Larry'}
+];
+
+const repeatSalesEntries = [
+    {customerId : 1,
+    orderId : 1,
+    total: 3},
+    {customerId : 2,
+    orderId : 2,
+    total : 4 },
+    {customerId : 1,
+    orderId : 4,
+    total: 7},
+    {customerId : 2,
+    orderId : 4,
+    total : 5 }
+];
+
+const repeatCustomerEntries = [
+    {id : 1,
+    name: 'Moe'},
+    {id : 2,
+    name: 'Larry'},
+    {id : 1,
+    name: 'Moe'},
+    {id : 2,
+    name: 'Larry'}
+];
+
+describe('Customer Sales Map function', function(){
+    it('checks that the generateCustomerSalesMap is defined', function(){
+      expect( generateCustomerSalesMap).to.be.ok;
+    });
+    it('returns an object', function() {
+        expect( generateCustomerSalesMap([], [])).to.eql( {} );
+    });
+    it('combines simple sales data', function() {
+        expect(generateCustomerSalesMap(sales, customers)).to.eql({'Moe' : 3, 'Larry' : 4})
+    });
+    it('accumulates multiple sales entries for each customer', function(){
+        expect(generateCustomerSalesMap(repeatSalesEntries, customers)).to.eql({'Moe' : 10, 'Larry' : 9})
+    });
+    it('combines multiple customer entries ', function(){
+        expect(generateCustomerSalesMap(repeatSalesEntries,repeatCustomerEntries)).to.eql({'Moe' : 10, 'Larry' : 9})
+    });
+    it('ignores customers with no name', 
+    function() {
+        expect(generateCustomerSalesMap(sales, [{id: 1, name: undefined}])).to.eql({})
+});
+  });
---
+++ test/acme.js
@@ -0,0 +1,25 @@
+//Goal is to return an object where the keys are the customer names and the values are the total sales for that customer.
+
+const generateCustomerSalesMap = (salesData, customerData) => {
+    const customerSalesData = {};
+
+    //Add each customer to the customerSalesData object and then check the salesData array for all the customer's sales
+    customerData.forEach(function(customer) {
+        //ignore customer files with no name
+        if(!customer.name) {
+            return;
+        }
+
+        //add customers to the new object and calculate their total sales
+        customerSalesData[customer.name] = salesData.reduce(function(accumulator, saleInfo){
+            if(saleInfo.customerId === customer.id) {
+                return saleInfo.total + accumulator;
+            }
+            return accumulator;
+        },0)
+    });
+    return customerSalesData;
+}
+
+
+module.exports = generateCustomerSalesMap;
